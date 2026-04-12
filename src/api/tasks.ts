6f287da680f72d96881ea7a70cce41736c48c14a
Dhivakar <thenameisdhivakar@gmail.com>
2026-04-12 23:07:47 +0530

    Added API backend

---
+++ src/api/tasks.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { Task } from "../types";
+
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
+
+export const getTasks = async (): Promise<Task[]> => {
+    const res = await axios.get(API_URL);
+
+    return res.data.slice(0, 30).map((task: any) => {
+        return {
+            userId: task.userId,
+            id: task.id,
+            title: task.title,
+            completed: task.completed,
+            priority: "low",
+        };
+    });
+};
